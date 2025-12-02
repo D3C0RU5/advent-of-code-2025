1f84cd224a4bb53ba4ab0f5ed8dfa5094f00cb8d
Carlos Henrique Pedrada Peterle <che@certi.org.br>
2025-12-02 10:16:36 -0300

    first commit

---
+++ day-01-p1/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INPUT_LIMIT 8
+#define MOD 100
+#define START_POSITION 50
+
+int extractSteps(char *word)
+{
+    char number_as_string[INPUT_LIMIT] = {0};
+    int i = 0;
+
+    word++;
+
+    while (*word != '\n' && *word != '\0' && i < INPUT_LIMIT - 1)
+    {
+        number_as_string[i++] = *word++;
+    }
+
+    return (int)strtol(number_as_string, NULL, 10);
+}
+
+int main()
+{
+    FILE *fp = fopen("./input.txt", "r");
+    if (!fp)
+    {
+        perror("Error opening file.");
+        return 1;
+    }
+
+    int position = START_POSITION;
+
+    char line[INPUT_LIMIT];
+    int zero_counter = 0;
+    while (fgets(line, INPUT_LIMIT, fp))
+    {
+        int next_position;
+        int steps = extractSteps(line) % MOD;
+        char direction = line[0];
+
+        if (direction == 'R')
+        {
+            next_position = (position + steps) % MOD;
+            printf("position[R]: %d + %d => ", position, steps);
+            position = next_position;
+            printf("%d.\n", position);
+        }
+        else if (direction == 'L')
+        {
+            next_position = (position - steps + MOD) % MOD;
+            printf("position[L]: %d - %d => ", position, steps);
+            position = next_position;
+            printf("%d.\n", position);
+        }
+        else
+        {
+            fprintf(stderr, "Error reading. Char: '%c' - ", line[0]);
+            return 1;
+        }
+        if (position == 0)
+            zero_counter++;
+    }
+    printf("🔑 ANSWER -> %d\n", zero_counter);
+
+    fclose(fp);
+    return 0;
+}
---
+++ day-01-p2/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define INPUT_LIMIT 32
+#define MOD 100
+#define START_POSITION 50
+#define MINIMUM_VALUE 0
+typedef enum
+{
+    DIR_LEFT = 'L',
+    DIR_RIGHT = 'R'
+} Direction;
+
+int extractSteps(const char *word)
+{
+    word++;
+
+    char buffer[INPUT_LIMIT] = {0};
+    int i = 0;
+
+    while (*word && *word != '\n' && i < INPUT_LIMIT - 1)
+    {
+        if (!isdigit(*word))
+            break;
+        buffer[i++] = *word++;
+    }
+
+    char *end;
+    long value = strtol(buffer, &end, 10);
+
+    if (end == buffer)
+    {
+        fprintf(stderr, "Error: invalid number founded.\n");
+        return 0;
+    }
+
+    return (int)value;
+}
+
+int moveCircular(int position, int steps, int direction)
+{
+    int newPos = (position + direction * steps) % MOD;
+    if (newPos < 0)
+        newPos += MOD;
+    return newPos;
+}
+
+int crossesZero(int oldPos, int newPos, int direction)
+{
+    if (direction == +1)
+        return newPos < oldPos;
+    else
+        return newPos > oldPos;
+}
+
+int main()
+{
+    FILE *fp = fopen("./input.txt", "rb");
+    if (!fp)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int position = START_POSITION;
+    int zero_counter = 0;
+
+    char line[INPUT_LIMIT];
+    while (fgets(line, INPUT_LIMIT, fp))
+    {
+
+        if (line[0] != DIR_RIGHT && line[0] != DIR_LEFT)
+        {
+            fprintf(stderr, "Linha inválida: %s\n", line);
+            continue;
+        }
+
+        int direction = (line[0] == DIR_RIGHT) ? +1 : -1;
+
+        int raw_steps = extractSteps(line);
+        int wraps = raw_steps / MOD;
+        int steps = raw_steps % MOD;
+
+        if (wraps > 0)
+            zero_counter += wraps;
+
+        int newPos = moveCircular(position, steps, direction);
+
+        if (crossesZero(position, newPos, direction))
+            zero_counter++;
+
+        position = newPos;
+    }
+
+    fclose(fp);
+
+    printf("🔑 ANSWER -> %d\n", zero_counter);
+    return 0;
+}
